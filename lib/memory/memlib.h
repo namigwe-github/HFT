dc460f8b5b343614a9db9bea523926d4077d0724
Nam I <namigwe.dev@gmail.com>
2024-02-26 12:58:45 -0500

    Memlib update

--- lib/memory/memlib.h
+++ lib/memory/memlib.h
@@ -7,12 +7,7 @@ namespace hft{
         template <typename T>
         class MemPool{
         public:
-            struct MemBlock{
-                T data;
-                bool isFree = true;
-            };
-
-            MemPool(size_t size): m_size(size), freeIndex(0){
+            MemPool(size_t size): m_size(size), freeIndex(0), isFull(false), totalFreeInds(size){
                 pool = std::vector<MemBlock>(size);
             }
             ~MemPool(){}
@@ -21,25 +16,52 @@ namespace hft{
                 T* allocmem = reinterpret_cast<T*>(&(pool[freeIndex]));
                 auto vec = {args...};
                 bool temp;
+                
                 for(auto v:vec) {
-                    temp = pool[freeIndex].isFree;
-                    if (temp){
+                    if (totalFreeInds){
                         pool[freeIndex].data = v;
                         pool[freeIndex].isFree = false;
-                        if (freeIndex == (m_size)) break;
-                        freeIndex++;
+                        totalFreeInds--;
+                        updateFreeIndex();
                     } else {
-                        while(!freeIndex) 
+                        std::cout << "Memory pool full, element not added\n";
+                        return nullptr;
                     }
                 }
-
+                 
                 return allocmem;
-
             }
+
+            void updateFreeIndex() {
+                auto origFreeIndex = freeIndex;
+                while(!pool[freeIndex].isFree) {
+                    freeIndex++;
+                    if (freeIndex == m_size) {
+                        freeIndex = 0;
+                    }
+
+                    if (freeIndex == origFreeIndex){
+                        std::cout << "Memory pool full\n";
+                        isFull = true;
+                        break;
+                    }
+                }
+            }
+
             void deallocate(T* ptr){
                 auto ind = reinterpret_cast<MemBlock*>(ptr) - &(pool[0]);
-                std::cout << "Index to deallocate " << ind << "\n";
-                pool[ind].isFree = true;
+
+                //std::cout << "Index to deallocate " << ind << "\n";
+                if (!pool[ind].isFree) {
+                    pool[ind].isFree = true;
+                    totalFreeInds++;
+                    if(isFull) {
+                        freeIndex = ind;
+                        isFull = false;
+                    }
+                } else {
+                    std::cout << "Memory already deallocated\n";
+                }
             }
 
             size_t getSize() {
@@ -56,13 +78,31 @@ namespace hft{
                 }
             }
 
+            bool isPoolFull() {
+                return isFull;
+            }
+
+            size_t getFreeInds() {
+                return totalFreeInds;
+            }
+
+            T& operator[](size_t index){
+                return pool[index].data;
+            }
             
         private:
+            struct MemBlock{
+                T data;
+                bool isFree = true;
+            };
+
             std::vector<MemBlock> pool;
             void* m_pool;
             size_t m_size;
             size_t freeIndex;
-        
+            bool isFull;
+            size_t totalFreeInds;
+
         };
     }
 }
--- tests/lib/testmemlib.cpp
+++ tests/lib/testmemlib.cpp
@@ -2,22 +2,64 @@
 #include <memlib.h>
 #include <algorithm>
 
+
+
 TEST(MemPool, Allocate){
     hft::memory::MemPool<int> pool(10);
-    //hft::memory::MemPool<int>::MemBlock* v = pool.allocate(7,9,5,10,2);
-    std::cout << "Here\n";
-    //int* v = pool.allocate(7,9,5,10,2);
-    std::vector<int> vec = {7,9,5,10,2};
-    pool.printPool();
-    std::for_each(vec.begin(), vec.end(), [&pool](int i){
-            //std::cout << *(pool.allocate(i)) << "\n";
-            pool.deallocate((pool.allocate(i)));
+    ASSERT_EQ(pool.getFreeInds(), 10);
+    
+    
+    std::vector<int> vec = {7,9,5,10,2,3,4,5,6,7,9,10,11,12,13,14,15,16,17,18,19,20};
+    std::vector<int*> ptrs;
+    
+    
+    std::for_each(vec.begin(), vec.end(), [&pool, &ptrs](int i){
+            if(!pool.isPoolFull()) ptrs.push_back(pool.allocate(i));
         });
-    /*for(size_t i=0; i<pool.getSize(); i++) {
-        std::cout << *v <<"\n";
-    }*/
+
+    int i = 0;
+    std::for_each(ptrs.begin(), ptrs.end(), [&pool, &i](int* ptr){
+            ASSERT_EQ(*ptr, pool[i]);
+            i++;
+        });
+    ASSERT_EQ(pool.isPoolFull(), true);
+    ASSERT_EQ(pool.getFreeInds(), 0);
     pool.printPool();
-    std::cout << "Rebuilt again\n";
+    std::cout << "\n";
+}
+
+TEST(MemPool, DeallocateAndReallocate){
+    hft::memory::MemPool<int> pool(10);
+ 
+    std::vector<int> vec = {7,9,5,10,2,3,4,5,6,7,9,10,11,12,13,14,15,16,17,18,19,20};
+    std::vector<int*> ptrs;
+    
+    std::for_each(vec.begin(), vec.end(), [&pool, &ptrs](int i){
+            if(!pool.isPoolFull()) ptrs.push_back(pool.allocate(i));
+        });
+    int a = 0;
+    std::for_each(ptrs.begin(), ptrs.end(), [&pool, &a ](int* ptr){
+            if(a%2 == 0) pool.deallocate(ptr);
+            a++;
+        });
+    ASSERT_EQ(pool.getFreeInds(), 5);
+    ASSERT_EQ(pool.isPoolFull(), false);
+
+     vec = std::vector<int>{200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100};
+    std::for_each(vec.begin(), vec.end(), [&pool, &ptrs](int i){
+            if(!pool.isPoolFull()) ptrs.push_back(pool.allocate(i));
+        });
+
+    ASSERT_EQ(ptrs[10], &pool[0]);
+    ASSERT_EQ(ptrs[11], &pool[2]);
+    ASSERT_EQ(ptrs[12], &pool[4]);
+    ASSERT_EQ(ptrs[13], &pool[6]);
+    ASSERT_EQ(ptrs[14], &pool[8]);
+    
+
+    ASSERT_EQ(pool.isPoolFull(), true);
+    pool.printPool();
+    std::cout << "\n";
 
 }
 
